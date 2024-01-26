ce07e7d26a03af87f9017ce0bf9be12afb6d13d7
Your name <Your email>
2024-01-26 11:38:35 +0100

    ff

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-fetch('https://whatyearisit-backend-xi-dun.vercel.app/date')
+fetch('https://whatyearisit-backend-xi-dun.vercel.app/year')
     .then(response => response.json())
     .then(data => {
         console.log(data)
